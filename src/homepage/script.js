f7f571453a830dc552818ad03e70431a7042d129
Vaibhav Mishra <vmishra175@gmail.com>
2021-11-03 12:50:51 +0530

    added jquery and ajax using data.json

--- src/homepage/script.js
+++ src/homepage/script.js
@@ -1,17 +1,46 @@
 console.log("Loaded for homepage");
 
 function changeNavBar() {
-    const headerWrapper = document.getElementById("header_wrapper");
-    if(this.scrollY > 10){
-        headerWrapper.classList.add('fixed_bar');
-    } else {
-        headerWrapper.classList.remove('fixed_bar');
-    }
+  const headerWrapper = document.getElementById("header_wrapper");
+  if (this.scrollY > 10) {
+    headerWrapper.classList.add("fixed_bar");
+  } else {
+    headerWrapper.classList.remove("fixed_bar");
+  }
 }
 
-document.getElementById('service_button').addEventListener('click', () => {
-    console.log('Clicked')
-    document.getElementById('service_wrapper').scrollIntoView({ block: 'start',  behavior: 'smooth' });
+document.getElementById("service_button").addEventListener("click", () => {
+  console.log("Clicked");
+  document
+    .getElementById("service_wrapper")
+    .scrollIntoView({ block: "start", behavior: "smooth" });
 });
 
-window.addEventListener('scroll', changeNavBar, false);
+window.addEventListener("scroll", changeNavBar, false);
+
+const xhr = new XMLHttpRequest();
+
+$(document).ready(function () {
+  $.ajax({
+    url: "data.json",
+    success: function (data) {
+      for (let i = 0; i < data.length; i++) {
+        $(".pet-section").append(` <div class="pet-adoption">
+        <div class="img">
+          <img
+            src=${data[i].img}
+            alt="img"
+          />
+        </div>
+        <div class="info">
+        <h4>${data[i].name}</h4>
+        <p>Gender : ${data[i].Gender}</p>
+        <p>City : ${data[i].City}</p>
+        <p>Join Date : ${data[i].JoinDate}</p>
+        <p>Status : ${data[i].Status}</p>
+        </div>
+      </div>`);
+      }
+    },
+  });
+});
